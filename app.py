ffb4c83bef22d1bbe4b414fd05007d0f61f74231
shahroznaveed <shahroznaveed003@gmail.com>
2025-05-10 23:46:10 +0500

    add debug_compression route for inspecting compression and session data integrity

--- app.py
+++ app.py
@@ -344,7 +344,6 @@ def download_report():
         return "Website parameter is required", 400
     return create_seo_report(website)
 
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
